bece48ff33547eb829669b93f777ecabcea5d955
Denys Misko <gdraal@gmail.com>
2014-08-04 11:00:09 +0300

    Fixed rpmbuild script git version bug

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo Building ${NAME} RPMs..
-GITROOT=$(git rev-parse --show-toplevel)
-cd $GITROOT
 
-REPO=$(git config --get remote.origin.url)
+PROJECT_PATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
-REL=$(git rev-parse --short HEAD)git
-RPMTOPDIR=$GITROOT/rpm-build
-echo "Ver: $VER, Release: $REL, Repo: $REPO"
+if [  -z "$REPO" ]; then
+	REPO=$(git config --get remote.origin.url)
+fi
+
+RPMTOPDIR=$PROJECT_PATH/rpm-build
+
 
 rm -rf $RPMTOPDIR
 
@@ -16,24 +17,27 @@ rm -rf $RPMTOPDIR
 mkdir -p $RPMTOPDIR/{SOURCES,SPECS}
 
 
-git clone --recursive --depth 1 ${REPO} $RPMTOPDIR/SOURCES/${NAME}-${VER}-${REL}
-cd $RPMTOPDIR/SOURCES/${NAME}-${VER}-${REL}/
-sh autogen.sh
-cd ..
-tar -czf ${NAME}-${VER}-${REL}.tar.gz ${NAME}-${VER}-${REL}
-cd $GITROOT
-
-# Convert git log to RPM's ChangeLog format (shown with rpm -qp --changelog <rpm file>)
+git clone --recursive --depth 1 ${REPO} $RPMTOPDIR/SOURCES/${NAME}-${VER}
+cd $RPMTOPDIR/SOURCES/${NAME}-${VER}/
+REL=$(git rev-parse --short HEAD)git
+echo "Ver: $VER, Release: $REL, Repo: $REPO, Path: $RPMTOPDIR"
 sed -e "s/%{ver}/$VER/" -e "s/%{rel}/$REL/" packaging/${NAME}.spec > $RPMTOPDIR/SPECS/${NAME}.spec
 #git log --format="* %cd %aN%n- (%h) %s%d%n" --date=local | sed -r 's/[0-9]+:[0-9]+:[0-9]+ //' >> $RPMTOPDIR/SPECS/${NAME}.spec
+
+sh autogen.sh
+
+cd ..
+mv ${NAME}-${VER} ${NAME}-${VER}-${REL}
+tar -czf ${NAME}-${VER}-${REL}.tar.gz ${NAME}-${VER}-${REL}
+cd $PROJECT_PATH
 
 # make RPMs directory
 mkdir -p RPMS
 
 # Build SRC and binary RPMs
 rpmbuild		--define "_topdir $RPMTOPDIR" \
-            --define "_rpmdir $PWD/RPMS"       \
-            --define "_srcrpmdir $PWD/RPMS"    \
+            --define "_rpmdir $PROJECT_PATH/RPMS"       \
+            --define "_srcrpmdir $PROJECT_PATH/RPMS"    \
             --define '_rpmfilename %%{NAME}-%%{VERSION}-%%{RELEASE}.%%{ARCH}.rpm' \
             -ba $RPMTOPDIR/SPECS/${NAME}.spec &&
 
