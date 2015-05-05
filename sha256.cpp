82950c88411ac352182846abb3d7be57a6ab4f97
Denys Misko <gdraal@gmail.com>
2015-05-05 23:02:10 +1000

    Added 498 token expire http error`

--- sha256.cpp
+++ sha256.cpp
@@ -10,6 +10,7 @@
 
 
 #include "sha256.hpp"
+#include "util.hpp"
 #include <cstring>
 
 using namespace fl::crypto;
@@ -22,4 +23,9 @@ SHA256Holder::SHA256Holder()
 SHA256Holder::SHA256Holder(const std::string &data)
 {
 	SHA256(reinterpret_cast<const unsigned char*>(data.c_str()), data.size(), _bytes);
+}
+
+void SHA256Holder::setHex(const char *hex, size_t len)
+{
+	fl::utils::hex2Binary(hex, _bytes, SHA256_DIGEST_LENGTH);
 }
